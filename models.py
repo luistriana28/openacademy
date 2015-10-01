c42fd0c63ebcf3b6c251360bedf20a6a2a2f7f4d
Luis Triana <luistriana.28@gmail.com>
2015-10-01 18:01:44 -0500

    Delete controllers.py and a good structure in models dir

--- models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-class Course(models.Model):
-    _name = 'openacademy.course'
-
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
