e4392cdac49411c27c53b317eebf1c2550b9cfc1
Luis Triana <luistriana.28@gmail.com>
2015-10-01 18:03:41 -0500

    Adding new files in models dir

---
+++ models/openacademy_course.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class OpenacademyCourse(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
