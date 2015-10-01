c0715c44f3b56a20a42b966a996f033c9b3763f8
Luis Triana <luistriana.28@gmail.com>
2015-10-01 13:34:35 -0500

    First Commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{   
+    "name"        : "OpenAcademy",
+    "version"     : "1.0",
+    "category"    : "Test",
+    "author"      : "Jarsa Sistemas, S.A. de C.V.",
+    "website"     : "www.jarsa.com.mx",
+    "depends"     : ['base'],
+    "summary"     : "Open Academy modulo for managin trainings",
+    "data" : [	'templates.xml', 
+    			'views/openacademy.xml'
+    		 ],
+    "demo" : ['demo.xml'],
+    "application": True,
+    "installable": True,
+}
