20279a9900e034f40c3ee03813e41ccc743700ee
Luis Triana <luistriana.28@gmail.com>
2016-02-08 11:48:55 -0600

    [ADD] change in README

--- models/openacademy_course.py
+++ models/openacademy_course.py
@@ -1,10 +1,12 @@
+
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields
+
 
 class OpenacademyCourse(models.Model):
+    """First class in the module."""
     _name = 'openacademy.course'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-
